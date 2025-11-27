00de3f5dd3a10f4501b426069b87507a9d97eeb0
RaihanKabir277 <raihank0192@gmail.com>
2025-11-27 08:49:59 +0000

    consumer added here

---
+++ consumer/consumer.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer(
+    "fraud_alerts",
+    bootstrap_servers="localhost:9092",
+    auto_offset_reset="earliest",
+    value_deserializer=lambda m: m.decode("utf-8")
+)
+
+print("Listening for fraud alerts...\n")
+
+for msg in consumer:
+    print(msg.value)
